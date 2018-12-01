dad2dc39b1e705a3e2a6b92c0387308b3910df4e
Lucas Polo <pradopolo@gmail.com>
2018-12-01 19:32:08 -0200

    Alterado formulário para o Spring gerenciar
    close #3

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -32,7 +32,7 @@ public class ProdutosController {
 	}
 	
 	@RequestMapping("/form")
-	public ModelAndView form() {
+	public ModelAndView form(Produto produto) {
 		ModelAndView modelAndView = new ModelAndView("produtos/form");
 		modelAndView.addObject("tipos", TipoPreco.values());
 		System.out.println("Retornando modelAndView");
@@ -43,7 +43,7 @@ public class ProdutosController {
 	public ModelAndView gravar(@Valid Produto produto, BindingResult result, RedirectAttributes redirectAttributes) {
 
 		if (result.hasErrors()) {
-			return form();
+			return form(produto);
 		}
 		
 		produtoDAO.gravar(produto);
