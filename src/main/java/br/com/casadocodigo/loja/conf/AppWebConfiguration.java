dac9c6583f2d63a5c9485a6be652a4bfbe4f73e1
Lucas Polo <pradopolo@gmail.com>
2018-12-01 19:21:00 -0200

    Inserida data de lançamento do livro
    close #1

--- src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -4,6 +4,11 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.format.datetime.DateFormatter;
+import org.springframework.format.datetime.DateFormatterRegistrar;
+import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.format.support.FormattingConversionService;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -31,5 +36,15 @@ public class AppWebConfiguration {
 		messageSource.setCacheSeconds(1);
 		
 		return messageSource;
+	}
+	
+	@Bean
+	public FormattingConversionService mvcConversionService() {
+		DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+		DateFormatterRegistrar dateFormatterRegistrar = new DateFormatterRegistrar();
+		dateFormatterRegistrar.setFormatter(new DateFormatter("dd/MM/yyyy"));
+		dateFormatterRegistrar.registerFormatters(conversionService);
+		
+		return conversionService;
 	}
 }
